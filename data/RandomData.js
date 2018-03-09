8d9e4068279463c33c72c1d05972d7e0d1f21916
mickey-ichi <lemanhhungb01501@gmail.com>
2018-03-09 10:31:16 +0700

    update random data

--- data/RandomData.js
+++ data/RandomData.js
@@ -14,10 +14,24 @@ class RandomData {
             date : date,
             timestamp: start + delta,
             value: {
-                country : ['JP', 'US', 'CH'][Math.round(Math.random() * 100) % 2],
-                value : Math.floor(Math.random() * 10)
+              country: ["JP","CN","EN"][ parseInt(Math.random() * 3)],
+              category : ["A","B"][ parseInt(Math.random() * 2) ],
+              os_flow : parseInt(Math.random() * 6000),
+              ow_flow : parseInt(Math.random() * 3000),
+              pv : parseInt(Math.random() * 2000),
+              ss : parseInt(Math.random() * 2000),
+              uu : parseInt(Math.random() * 2000),
+              value : Math.floor(Math.random() * 10),
+              node_id : "00000" + parseInt(Date.now()/100).toString().substr(-4),
+              created: Date.now(),
+              latitude: RandomData.getRandomInt(90, -90),
+              longitude: RandomData.getRandomInt(180, -180),
             }
         }
+    }
+
+   static getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
 }
